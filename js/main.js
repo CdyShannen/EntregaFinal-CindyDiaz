330821cb1827e8ebcc6fac12dfd22bafde89d471
“Cindy <cindysdiaza@gmail.com>
2023-12-10 16:13:43 -0300

    Ultimas modificaciones del carrito

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ let modalText = document.getElementById("content-modal");
 
 class Producto {
   constructor(num, nombre, precio, cantidad) {
-    this.id = num;
+    this.num = num;
     this.nombre = nombre;
     this.precio = precio;
     this.cantidad = cantidad;
@@ -13,11 +13,9 @@ class ProductoController {
   constructor() {
     this.listadeproductos = [];
   }
-
   colocar(producto) {
     this.listadeproductos.push(producto);
   }
-
   cargarProductosJSON() {
     fetch("../js/api.json")
       .then((Response) => Response.json())
@@ -45,7 +43,7 @@ class NuevaCompra {
     this.productos = [];
   }
 
-  agregarProducto(productos, productoNum) {
+agregarProducto(productos, productoNum) {
     const productoExistente = this.productos.find(
       (producto) => producto.num === productoNum
     );
@@ -60,13 +58,13 @@ class NuevaCompra {
     }
     localStorage.setItem("productos", JSON.stringify(this.productos));
   }
- 
-  descripcion() {
+
+descripcion() {
     let html = `<div>`;
     let total = 0;
     this.productos.forEach((producto, index) => {
       html += `<div class='card'>
-				<h3>${producto.nombre} ${producto.capacidad}</h3>
+				<h3>${producto.nombre}</h3>
 				<p>Precio: ${producto.precio}$</p>
 				<button class="suma-resta" onclick="carritoInstance.sumar(${index})">+</button>
 				<input type="number" class="input-cantidad" min="0" value="${producto.cantidad}" readonly>
@@ -91,13 +89,14 @@ if (carritoData) {
 
 productos.cargarProductosJSON();
 
+let productosCargados = false; 
+
 class Carrito {
   sumar(index) {
     const producto = miNuevaCompra.productos[index];
     producto.cantidad += 1;
     modalText.innerHTML = miNuevaCompra.descripcion();
   }
-
   restar(index) {
     const producto = miNuevaCompra.productos[index];
     if (producto.cantidad > 0) {
@@ -105,7 +104,6 @@ class Carrito {
       modalText.innerHTML = miNuevaCompra.descripcion();
     }
   }
-
   finalizarCompra() {
     let finalizar = document.getElementById("btn_fin");
     finalizar.addEventListener("click", function () {
@@ -113,7 +111,6 @@ class Carrito {
         modalText.innerHTML = "";
         miNuevaCompra.productos = [];
         localStorage.setItem("productos", JSON.stringify(miNuevaCompra.productos));
-
         Toastify({
           text: "Compra realizada con éxito, ¡gracias por comprar con nosotros!",
           duration: 3000,
@@ -130,23 +127,20 @@ class Carrito {
 
 //Obtener botones
 const agregarEventos = () => {
-
+  if (!productosCargados) {
   // Botones de plantas
   const btn1 = document.getElementById("btn1");
   const btn2 = document.getElementById("btn2");
   const btn3 = document.getElementById("btn3");
   const btn4 = document.getElementById("btn4");
-
   btn1.addEventListener("click", () => {
     miNuevaCompra.agregarProducto(productos, "1");
     modalText.innerHTML = miNuevaCompra.descripcion();
-
     Toastify({
       text: "Producto añadido exitosamente",
       duration: 3000,
     }).showToast();
   });
-
   btn2.addEventListener("click", () => {
     miNuevaCompra.agregarProducto(productos, "2");
     modalText.innerHTML = miNuevaCompra.descripcion();
@@ -155,7 +149,6 @@ const agregarEventos = () => {
       duration: 3000,
     }).showToast();
   });
-
   btn3.addEventListener("click", () => {
     miNuevaCompra.agregarProducto(productos, "3");
     modalText.innerHTML = miNuevaCompra.descripcion();
@@ -164,7 +157,6 @@ const agregarEventos = () => {
       duration: 3000,
     }).showToast();
   });
-
   btn4.addEventListener("click", () => {
     miNuevaCompra.agregarProducto(productos, "4");
     modalText.innerHTML = miNuevaCompra.descripcion();
@@ -173,12 +165,10 @@ const agregarEventos = () => {
       duration: 3000,
     }).showToast();
   });
-
   // Botones de maceta
   const btn5 = document.getElementById("btn5");
   const btn6 = document.getElementById("btn6");
   const btn7 = document.getElementById("btn7");
-
   btn5.addEventListener("click", () => {
     miNuevaCompra.agregarProducto(productos, "5");
     modalText.innerHTML = miNuevaCompra.descripcion();
@@ -187,7 +177,6 @@ const agregarEventos = () => {
       duration: 3000,
     }).showToast();
   });
-
   btn6.addEventListener("click", () => {
     miNuevaCompra.agregarProducto(productos, "6");
     modalText.innerHTML = miNuevaCompra.descripcion();
@@ -196,7 +185,6 @@ const agregarEventos = () => {
       duration: 3000,
     }).showToast();
   });
-
   btn7.addEventListener("click", () => {
     miNuevaCompra.agregarProducto(productos, "7");
     modalText.innerHTML = miNuevaCompra.descripcion();
@@ -204,8 +192,11 @@ const agregarEventos = () => {
       text: "Producto añadido exitosamente",
       duration: 3000,
     }).showToast();
-  });
-
+  }); 
+  productosCargados = true;}
 };
 
+
 agregarEventos();
+const carritoInstance = new Carrito();
+carritoInstance.finalizarCompra();
